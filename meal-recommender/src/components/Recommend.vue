11209385b69cd8ed2d7e09bcbc3ed0ee196a54a7
Ranvin36 <141607057+Ranvin36@users.noreply.github.com>
2025-02-25 23:38:29 +0530

    Project Setup

---
+++ meal-recommender/src/components/Recommend.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <h1>aa</h1>
+
+</template>
---
+++ meal-recommender/src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Recommender from '../components/Recommend.vue'
+</script>
+
+<template>
+  <main>
+    <Recommender />
+  </main>
+</template>
